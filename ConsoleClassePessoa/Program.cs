dc6c93cd80ad0dabd14bdbee1c14dfb0f1a858a4
Wallace Silva <wleitte@outlook.com>
2021-10-14 19:10:51 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleClassePessoa/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleClassePessoa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
